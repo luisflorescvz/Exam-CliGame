d99ef5e5e8ccb703750ed1531487e6ac1ef40ca8
luisflorescvz <alberto.flores@iest.edu.mx>
2024-04-17 20:15:38 -0600

    The file was added to Git

---
+++ index.js
@@ -0,0 +1,157 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'How well do you know me? \n'
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')} 
+    I am a process on your computer.
+    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    So get all the questions right...
+
+  `);
+}
+
+async function askName() {
+    const answers = await inquirer.prompt({
+      name: 'player_name',
+      type: 'input',
+      message: 'What is your name?',
+      default() {
+        return 'Player';
+      },
+    });
+  
+    playerName = answers.player_name;
+  }
+
+  async function question1() {
+    const answers = await inquirer.prompt({
+      name: 'Question-1',
+      type: 'list',
+      message: 'What is my favorite color?\n',
+      choices: [
+        'Red',
+        'Blue',
+        'Green',
+        'Yellow ',
+      ],
+    });
+  
+    return handleAnswer(answers.question_1 === 'Blue');
+  }
+
+  async function question2() {
+    const answers = await inquirer.prompt({
+      name: 'Question-2',
+      type: 'list',
+      message: 'What is my favorite game?\n',
+      choices: [
+        'Destiny2',
+        'ARK',
+        'LOL',
+        'StarCitizen ',
+      ],
+    });
+  
+    return handleAnswer(answers.question_1 === 'ARK');
+  }
+
+  async function question3() {
+    const answers = await inquirer.prompt({
+      name: 'Question-3',
+      type: 'list',
+      message: 'What do I like the most?\n',
+      choices: [
+        'Cars',
+        'Computers',
+        'Animals',
+        'Science ',
+      ],
+    });
+  
+    return handleAnswer(answers.question_1 === 'Cars');
+  }
+
+  async function question4() {
+    const answers = await inquirer.prompt({
+      name: 'Question-4',
+      type: 'list',
+      message: 'How much did my PC cost me?\n',
+      choices: [
+        '12k',
+        '17k',
+        '15k',
+        '10k ',
+      ],
+    });
+  
+    return handleAnswer(answers.question_1 === '15k');
+  }
+
+  async function question5() {
+    const answers = await inquirer.prompt({
+      name: 'Question-5',
+      type: 'list',
+      message: 'Am I going to pass this exam?\n',
+      choices: [
+        'Absolutely',
+        'Maybe',
+        'Maybe not',
+        'No chance ',
+      ],
+    });
+  
+    return handleAnswer(answers.question_1 === 'Maybe');
+  }
+
+
+  async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('Checking answer...').start();
+    await sleep();
+  
+    if (isCorrect) {
+      spinner.success({ text: `Nice work ${playerName}. That's a legit answer` });
+    } else {
+      spinner.error({ text: `💀💀💀 Game over, you lose ${playerName}!` });
+      process.exit(1);
+    }
+  }
+
+  function winner() {
+    console.clear();
+    figlet(`Congrats , ${playerName} !`, (err, data) => {
+      console.log(gradient.pastel.multiline(data) + '\n');
+
+      console.log(
+        chalk.green(
+          `I owe you your favorite thing in the world`
+        )
+      );
+  
+      process.exit(0);
+    });
+  }
+
+console.clear();
+await welcome();
+await askName();
+await question1();
+winner();
